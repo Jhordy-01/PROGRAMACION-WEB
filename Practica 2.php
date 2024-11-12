ca0b040e4b80f44f3ab66e8391be05ef3b436eae
Jhordy-01 <l21530364@cancun.tecnm.mx>
2024-11-12 15:05:08 -0500

    Add files via upload

---
+++ Practica 2.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+     <title>Mi Practica 2</title>
+    </head>
+    <body>
+        <?php
+         $a=10;
+         function Variables(){
+            $b=15;
+            echo "Prueba de la impresion de varibales dentro de no se que";
+            echo "La varibale a es $a <br>";
+            echo "La varibale b es $b <br><br>";
+         }
+
+         Variables();
+
+         echo "Prueba de la impresion de variables fuera de no se que <br>";
+         echo "La varibale a es $a <br>";
+         echo "La varibale b es $b <br>";
+         ?>
+        
+    </body>
+</html>
